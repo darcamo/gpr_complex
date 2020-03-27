6755ecfe0a724e6d9dcdf25913de953a12d24dcc
Darlan Cavalcante Moreira <darcamo@gmail.com>
2020-03-27 12:32:22 -0300

    Comment out test_plot
    The current code should not be run with CI

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -1,49 +1,47 @@
-import math
+# import math
 
-import numpy as np
-from bokeh.plotting import output_file
-# from sklearn.gaussian_process import GaussianProcessRegressor
-# from sklearn.gaussian_process.kernels import RBF as RBF_sklearn
-# from sklearn.gaussian_process.kernels import ConstantKernel, WhiteKernel
+# import numpy as np
+# from bokeh.plotting import output_file
+# # from sklearn.gaussian_process import GaussianProcessRegressor
+# # from sklearn.gaussian_process.kernels import RBF as RBF_sklearn
+# # from sklearn.gaussian_process.kernels import ConstantKernel, WhiteKernel
 
-from gpr_complex.kernels import RBF
-from gpr_complex.model import GPR
-from gpr_complex.plot import plot_gp
+# from gpr_complex.kernels import RBF
+# from gpr_complex.model import GPR
+# from gpr_complex.plot import plot_gp
 
-output_file("lalala.html")
+# output_file("lalala.html")
 
-noise_power = 0.5**2
+# noise_power = 0.5**2
 
+# def compute(x: np.ndarray) -> np.ndarray:  # pylint: disable=missing-function-docstring
+#     return 2.5 * x + math.sqrt(noise_power) * np.random.randn(*(x.shape))
 
-def compute(x: np.ndarray) -> np.ndarray:  # pylint: disable=missing-function-docstring
-    return 2.5 * x + math.sqrt(noise_power) * np.random.randn(*(x.shape))
+# x: np.ndarray = np.random.randn(200, 1)
+# y: np.ndarray = compute(x)
 
+# kernel = RBF(l=1.0, sigma_f=1.0)
+# gp = GPR(noise_power=noise_power, kernel=kernel)
+# gp.fit(x, y)
 
-x: np.ndarray = np.random.randn(200, 1)
-y: np.ndarray = compute(x)
+# # Sklearn GPR
+# # kernel2 = ConstantKernel() * RBF_sklearn() + WhiteKernel()
+# # gp2 = GaussianProcessRegressor(kernel=kernel2)
+# # gp2.fit(x, y)
 
-kernel = RBF(l=1.0, sigma_f=1.0)
-gp = GPR(noise_power=noise_power, kernel=kernel)
-gp.fit(x, y)
+# x_test = np.linspace(-1.5, 1.5, 30)[:, np.newaxis]
+# y_test = compute(x_test)
 
-# Sklearn GPR
-# kernel2 = ConstantKernel() * RBF_sklearn() + WhiteKernel()
-# gp2 = GaussianProcessRegressor(kernel=kernel2)
-# gp2.fit(x, y)
+# y_pred, cov_pred = gp.predict(x_test, return_cov=True)
+# # y_pred2, cov_pred2 = gp2.predict(x_test, return_cov=True)
 
-x_test = np.linspace(-1.5, 1.5, 30)[:, np.newaxis]
-y_test = compute(x_test)
+# # x_test = x_test.flatten()
+# # y_pred = y_pred.flatten()
 
-y_pred, cov_pred = gp.predict(x_test, return_cov=True)
-# y_pred2, cov_pred2 = gp2.predict(x_test, return_cov=True)
+# indexes = np.argsort(x_test.flatten())
 
-# x_test = x_test.flatten()
-# y_pred = y_pred.flatten()
-
-indexes = np.argsort(x_test.flatten())
-
-plot_gp(dict(x=x_test.flatten()[indexes],
-             mu=y_pred.flatten()[indexes],
-             cov=cov_pred),
-        dict(x=x.ravel(), y=y.ravel()),
-        samples=3)
+# plot_gp(dict(x=x_test.flatten()[indexes],
+#              mu=y_pred.flatten()[indexes],
+#              cov=cov_pred),
+#         dict(x=x.ravel(), y=y.ravel()),
+#         samples=3)
